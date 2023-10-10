7065b5c1ec4bedcb0fdc47154feb42aada4ac965
Duc Viet <pdvietnb@gmail.com>
2023-10-10 20:04:04 +0700

    translate

--- myweb_frontend/src/containers/HomePage/Header/HomeHeader.js
+++ myweb_frontend/src/containers/HomePage/Header/HomeHeader.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import "./HomeHeader.scss";
+import { FormattedMessage } from "react-intl";
+import { LANGUAGES } from "../../../utils";
+import { changeLanguage } from "../../../store/actions";
 
 class HomeHeader extends Component {
+  changeLanguage = (language) => {
+    this.props.changeLanguage(language);
+  };
   render() {
+    let language = this.props.language;
     return (
       <React.Fragment>
         <div className="header__container">
@@ -18,23 +25,35 @@ class HomeHeader extends Component {
             <div className="header__info">
               <div className="info__child pointer__event">
                 <i class="fas fa-shopping-cart"></i>
-                <div className="child__content">Giỏ hàng</div>
+                <div className="child__content">
+                  <FormattedMessage id="header__navbar.cart" />
+                </div>
               </div>
               <div className="info__child pointer__event">
                 <i class="fas fa-user"></i>{" "}
-                <div className="child__content">Tài khoản</div>
+                <div className="child__content">
+                  <FormattedMessage id="header__navbar.user" />
+                </div>
               </div>
             </div>
             <div className="header__language">
-              <div className="language language--vi pointer__event">VN</div>
-              <div className="language language--en pointer__event">EN</div>
+              <div className="language language--vi pointer__event">
+                <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>
+                  VN
+                </span>
+              </div>
+              <div className="language language--en pointer__event">
+                <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>
+                  EN
+                </span>
+              </div>
             </div>
           </div>
           <div className="header__menu">
             <div className="header__category pointer__event hover__event--blue">
               <i class="fas fa-bars"></i>
               <div className="option_content hover__event--bigger">
-                Danh mục sản phẩm
+                <FormattedMessage id="header__menu.category" />
               </div>
               <i class="fas fa-sort-down"></i>
             </div>
@@ -42,25 +61,25 @@ class HomeHeader extends Component {
               <div className="options pointer__event hover__event--blue">
                 <i class="fas fa-book"></i>
                 <div className="option_content option--hover hover__event--bigger">
-                  Tin tức
+                  <FormattedMessage id="header__menu.news" />
                 </div>
               </div>
               <div className="options pointer__event hover__event--blue">
                 <i class="fas fa-percent"></i>
                 <div className="option_content option--hover hover__event--bigger">
-                  Khuyến mãi
+                  <FormattedMessage id="header__menu.promotion" />
                 </div>
               </div>
               <div className="options pointer__event hover__event--blue">
                 <i class="fas fa-shield-alt"></i>
                 <div className="option_content option--hover hover__event--bigger">
-                  Bảo hành
+                  <FormattedMessage id="header__menu.warranty" />
                 </div>
               </div>
               <div className="options pointer__event hover__event--blue">
                 <i class="fas fa-phone-volume"></i>
                 <div className="option_content hover__event--bigger">
-                  Tư vấn
+                  <FormattedMessage id="header__menu.advisory" />
                 </div>
               </div>
             </div>
@@ -81,11 +100,14 @@ class HomeHeader extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    changeLanguage: (language) => dispatch(changeLanguage(language)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
